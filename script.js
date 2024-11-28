e4818e1f1415d2442fd56aaca9deaad1c4fdc69e
vivalife35 <vivalife35@gmail.com>
2024-11-28 11:02:39 +0300

    Added function update Progress

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const sleepData = { goal: 8, actual: 6 };
 const waterData = { goal: 2000, actual: 1500 };
 const walkingData = { goal: 10000, actual: 7500 };
+
+function updateProgress(goal, actual, progressElementId, trackerName) {
+    const percentage = Math.min((actual / goal) * 100, 100);
+    const progressMessage = `You have completed ${Math.round(percentage)}% of your ${trackerName} goal. Keep it up!`;
+    document.getElementById(progressElementId).textContent = progressMessage;
+  }
